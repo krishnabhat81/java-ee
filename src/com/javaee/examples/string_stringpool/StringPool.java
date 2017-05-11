85152cf649133d1d6b7f707e31e737cca7d37b14
Krishna Bahadur Bhat <krishna.bhat78@gmail.com>
2017-05-11 16:23:33 -0400

    String pool

---
+++ src/com/javaee/examples/string_stringpool/StringPool.java
@@ -0,0 +1,25 @@
+package com.javaee.examples.string_stringpool;
+
+/**
+ * Created by krishna1bhat on 5/11/17.
+ */
+
+//Strings are stored in Heap memory -- String is class....
+public class StringPool {
+    public static void main(String[] args){
+        //Using double quote
+        //First looks string with same value (Hello) in String pool
+        //if found just returns its reference, otherwise creates new String in pool and then returns  its reference
+
+        String s1 = "Hello";
+        String s2 = "Hello";
+
+        //Using new operator
+        //Creates new string object in heap memory (Hello)
+        String s3 = new String("Hello");
+
+        System.out.println("s1 == s2 : " + (s1 == s2) );
+        System.out.println("s2 == s3 : " + (s2 == s3) );
+
+    }
+}
